80cb2b7626b0d55b908b57c8d7298ded2ce15601
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2024-01-16 11:01:44 +0900

    백준 2738
    결과 : 실패
    
    1. 처음에 BW 출력 방식을 잘못해서 실패 IDE의 힘을 빌림
    2. 출력 형식이 잘못되었다고 함. 아마도 이 방식으로 푸는게 아닌듯

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        //3 3
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        //행렬 리스트 2개
+        List<Integer> AList = new ArrayList<Integer>();
+        List<Integer> BList = new ArrayList<Integer>();
+
+        //행렬1
+        for(int i = 0; i < N; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine(), " "); //1 1 1
+            for(int j = 0; j < M; j++){ //1 1 1
+                AList.add(Integer.parseInt(st2.nextToken()));
+            }
+        }
+
+        //행렬2
+        for(int i = 0; i < N; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine(), " "); //3 3 3
+            for(int j = 0; j < M; j++){ //3 3 3
+                BList.add(Integer.parseInt(st2.nextToken()));
+            }
+        }
+
+        //더해서 내보내기
+        for(int i = 0; i < N * M; i++){
+            bw.write(AList.get(i)+ BList.get(i) + " ");
+
+            if(i > 0 && i % N == 2){ //줄바꿈
+                bw.write("\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
