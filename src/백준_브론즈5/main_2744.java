b4b7029d24e3cc2abee7c91f0277c1f7e2c263b7
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2024-01-24 09:56:44 +0900

    백준 2744
    결과 : 재풀이 결과 성공
    
    - 그러나 배열을 굳이 사용해야 하는가
    - 아스키 코드를 이용해서 하나하나 분리해서 푸는것이 맞느냐 하는 문제 존재.

--- src/백준_브론즈5/main_2744.java
+++ src/백준_브론즈5/main_2744.java
@@ -2,43 +2,32 @@ package 백준_브론즈5;
 import java.io.*;
 
 /**
- * 첫번째 풀이는 char 배열로 변경한 뒤 아스키 코드를 이용해서
- * 소문자이면 + 32로 대문자로, 반대면 -32로 소문자로 바꾸는 방법을 이용
- *
- * 문제점은 : 굳이 배열을 사용해야 하느냐?
- * 아스키 코드를 사용하는 것이 맞느냐 하는 문제가 있었음
+ * 다른풀이 방법으로 한 번더 풀이
+ * Character class에 포함되어 있는 toLowerCase()와 toUpperCase()를 이용
  */
-
 public class main_2744 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringBuilder stb = new StringBuilder();
         String str = br.readLine();
-        char[] arr = new char[str.length()];
 
         for (int i = 0; i < str.length(); i++) {
-            arr[i] = str.charAt(i); //배열로 옮겨서 배열로 만들기
-        }
-
-        //변환
-        for (int i = 0; i < str.length(); i++) {
-            if (arr[i] < 97) { //소문자
-                arr[i] += 32; //대문자로
-            } else {
-                arr[i] -= 32; //소문자로
+            char c = str.charAt(i);
+            if(Character.isUpperCase(c)){ //대문자 -> 소문자
+                c = Character.toLowerCase(c); //변수로 저장해야함
+            }else{
+                c = Character.toUpperCase(c); //소문자 -> 대문자
             }
+            stb.append(c);
         }
 
-        //출력
-        for (int i = 0; i < str.length(); i++) {
-            bw.write(arr[i]);
-        }
-        bw.write("\n");
-
+        bw.write(stb + "\n");
         br.close();
         bw.flush();
-        br.close();
+        bw.close();
+
     }
 }
 
