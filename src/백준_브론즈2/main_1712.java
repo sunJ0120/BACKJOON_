7ca4770e8a8e1c40202a0459e660c8aec7cda823
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2024-02-01 09:27:52 +0900

    백준 1152~1712

---
+++ src/백준_브론즈2/main_1712.java
@@ -0,0 +1,14 @@
+package 백준_브론즈2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+BigInt 이런거 사용할 필요 없이 그냥 수식을 잘 구하면 되는 것이다..
+다시 한 번 더 보기!!!!!
+ */
+public class main_1712 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
