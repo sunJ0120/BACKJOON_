5195267d0f5e1c317b602b8c0903990e8241f570
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2024-02-06 10:00:41 +0900

    백준 2292
    결과 : 두 번 실패 후 성공
    
    규칙 찾고 while문 적용하는게 익숙치 않아서 상당히 애를 먹은 문제이다.

--- src/백준_브론즈2/main_1978.java
+++ src/백준_브론즈2/main_1978.java
@@ -10,22 +10,23 @@ n배열을 만들어서
 제곱근으로 이동하면서 2의배수..3의배수를 차례로 지워준다.
 그 다음에 주어진 수들을 읽어와서 소수인지 아닌지 판별하면 된다.
  */
+
 public class main_1978 {
     public static void main(String[] args) throws IOException {
         //----------------------
         /*
         에라토스테네스의 체 알고리즘
          */
-        boolean[] isPrime = new boolean[1000+1]; //소수 판별 배열 설정
-        Arrays.fill(isPrime, true); //초기값을 true로 설정
+        boolean[] isPrime = new boolean[1000+1];
+        Arrays.fill(isPrime,true); //전부 검사대상으로 변경
 
-        isPrime[0] = false; //0과 1은 소수가 아니므로
+        isPrime[0] = false; //0과 1은 소수가 아니다.
         isPrime[1] = false;
 
-        for (int i = 2; i * i <= 1000; i++) { //제곱근 까지 가는거고
-            if (isPrime[i]) {
-                for (int j = i*i; j <= 1000; j += i) { //j = i*i 이거인 이유가 3*n 이런 자신보다 작은수가 곱해져 있는 것은 이미 계산이 되었기 때문이다.
-                    isPrime[j] = false;
+        for(int i = 2; i*i <= 1000; i++){ //제곱근 까지만 검사하면 된다.
+            if(isPrime[i]){ //소수의 배수를 전부 지워야 하므로 소수인지 판별한다.
+                for(int j = i*i; j <= 1000; j+=i){ //i*i 이전은 i보다 작은 수의 배수이기 때문에 이미 검사가 끝난 이후이다.
+                    isPrime[j] = false; //소수가 아니다로 변경
                 }
             }
         }
@@ -33,20 +34,24 @@ public class main_1978 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int size = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(br.readLine());
         int ans = 0;
 
-        for (int i = 0; i < size; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
             int a = Integer.parseInt(st.nextToken());
-            if(isPrime[a] == true){
-                ans += 1;
+            if(isPrime[a]){ //true라면 ans+1
+                ans++;
             }
         }
+
         bw.write(ans + "\n");
         br.close();
         bw.flush();
         bw.close();
+
     }
 }
+
---
+++ src/백준_브론즈2/main_2292.java
@@ -0,0 +1,24 @@
+package 백준_브론즈2;
+
+import java.io.*;
+
+public class main_2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int n = 1; //겹수
+        int sum = 1; //끝번호
+
+        while(sum < num){
+            sum += n*6;
+            n++;
+        }
+
+        bw.write(n + "\n");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
