f3a7d4c639f857ee91776c58dfb18119538cd274
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2024-02-08 09:10:22 +0900

    백준 2163
    결과 : 성공
    
    1. 주어진 그대로 식을 쓰면 된다.
    2. 중요한 것은, ans와 n을 같게 두기 때문에 while문을 그냥 사용하면 안되고, do while을 이용해서 한 번 지나간 뒤에 규칙을 검사하도록 한다.

--- src/백준_브론즈2/main_2745.java
+++ src/백준_브론즈2/main_2745.java
@@ -3,22 +3,6 @@ package 백준_브론즈2;
 import java.io.*;
 import java.util.StringTokenizer;
 
-/*
-내가 너무 어렵게 접근했다 처음에...
-1. char로 쪼개고
-2. A의 경우는 65부터 시작이므로, 숫자로 바꾸려면 -55를 해주면 된다.
-3. 즉, 굳이 배열을 만들어 접근할 필요가 없다.
-
--'0'에 대해 잘못 이해하고 있었음
-
--'0'을 해주면 숫자로 바뀌눈 것은
-0이 아스키에서 48이기 때문에
-그걸 빼면 숫자 0이 나오기 때문이다.
-
-그런데 A이런애들은, 90이니까
--'0'을 빼면 42 이런식으로 나오겠지.
-그러니까 원하던 숫자값으로 나오지 않는 것이다!!
- */
 public class main_2745 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,23 +11,23 @@ public class main_2745 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         String str = st.nextToken();
-        int digit = Integer.parseInt(st.nextToken());
-        int ans = 0;
+        int n = Integer.parseInt(st.nextToken()); //진법
+        int ans = 0; //답
+        int digit = 0; //자릿수
 
-        for(int i = 0; i<= str.length()-1;i++){
+        for (int i = str.length()-1; i >=0 ; i--) { //일의 자리수부터 계산.
             char c = str.charAt(i);
             int num;
-            if('0'<=c && c<='9'){ //0~9
+            if (c >= '0' && c <= '9') { //십진수 까지의 숫자.
                 num = c - '0';
-
             }else{
-                num = c - 55;
+                num = c - 55; //A = 65
             }
-            ans += (Math.pow(digit, str.length() - 1 - i) * num);
+            ans += Math.pow(n,digit) * num;
+            digit++;
         }
 
         bw.write(ans + "\n");
-
         br.close();
         bw.flush();
         bw.close();
