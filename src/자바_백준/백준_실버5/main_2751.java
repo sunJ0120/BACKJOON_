63a47b1db6b63376c1aaf0d87d6873047a0ec53a
sunjung Oh <sspure1214@naver.com>
2024-02-17 18:52:58 +0900

    ✅자료구조 백준 2751 실버 5
    결과 : 어떤 분의 기발한 풀이를 보고 적용
    
    - 절댓값 1,000,000이라는 것을 이용해서, 1,000,000을 기준으로 잡고 true로 바꾸어 정렬 효과를 내도록 한것이 기발하다.
    - 다른 정렬 알고리즘과 다르게 서로 비교하는 부분이 없으므로 시간을 매우 많이 줄일 수 있다.
    (실제로 해보니 시간이 절반정도 절약되었다.)
    
    이 부분도 같이 정리 필요!

--- src/자바_백준/백준_실버5/main_2751.java
+++ src/자바_백준/백준_실버5/main_2751.java
@@ -1,15 +1,7 @@
 package 자바_백준.백준_실버5;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-/*
-정렬하기 문제
-기본적으로, 자바에는 arrays.sort가 있지 않나?
-이것말고 다른것을 사용하라는 의미인지는 모르겠지만, 일단 이걸 사용해보자.
- */
 public class main_2751
 {
     public static void main(String[] args) throws IOException {
@@ -18,13 +10,25 @@ public class main_2751
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        List<Integer> nlist = new ArrayList<Integer>(n);
+
+        /*
+        값이 -1,000,000 ~ 1,000,000이므로 1,000,000을 기준 인덱스로 두고, 값을 더하거나 빼서
+        작은값 - 큰값으로 정렬되게끔 한다.
+
+        직접 비교하는 것이 아니므로, 빠른 정렬이 가능하다.
+        중복되는 값이 없기 때문에 가능하다.
+         */
+        boolean[] tlist = new boolean[2000000+1];
+
         for (int i = 0; i < n; i++) {
-            nlist.add(Integer.parseInt(br.readLine()));
+            int num = Integer.parseInt(br.readLine());
+            tlist[num + 1000000] = true; //수를 저장
         }
-        Collections.sort(nlist);
-        for (int i = 0; i < n; i++) {
-            bw.write(nlist.get(i) + "\n");
+
+        for (int i = 0; i <= 2000000; i++) {
+            if(tlist[i]){
+                bw.write((i-1000000) + "\n");
+            }
         }
         bw.flush();
         br.close();
