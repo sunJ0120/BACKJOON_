871e0063ac2e3a679547aed6e3cff079a5e5da7e
sunjung Oh <sspure1214@naver.com>
2024-02-21 09:41:55 +0900

    ✅자료구조 백준 1094 실버 5
    결과 : 성공
    
    - 0보다 클때만 반복문을 시행할 것이므로 나눈 막대가 1보다 작아질 것이라는 걱정은 하지 않아도 된다.
    - 따로 기준점을 0으로 둘 필요 없이, 직접 빼주는 방식으로 가독성을 올린다.

--- src/자바_백준/백준_실버5/main_1094.java
+++ src/자바_백준/백준_실버5/main_1094.java
@@ -2,34 +2,38 @@ package 자바_백준.백준_실버5;
 
 import java.io.*;
 
+/*
+기존에는 0을 기준으로 잡았다.
+그럴 필요 없이, 주어진 막대 길이 자체를 기준으로 두고
+그게 0이 될때까지 시행하면 된다.
+ */
+
 public class main_1094 {
-    public static int st = 0;
-    public static int ans = 0;
+    public static int oriStick = 64;
+    public static int stick(int n){
 
-    public static int stick(int n, int m) { //bottom-up 방식으로
+        int count = 0;
 
-        while(n != st){
-            if(st + m <= n){ //더했을때 n보다 작거나 같으면 더할 수 있음, 아니면 막대만 나누고 넘어가기
-                st += m;
-                ans++;
-            }
-            if(m > 1){
-                m /= 2;
+        while(n > 0){
+            if(oriStick > n){
+                oriStick /= 2;
+            }else{
+                n -= oriStick;
+                count++;
             }
         }
-        return ans;
+        return count;
     }
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
-        bw.write(stick(n, 64) + "\n");
+        bw.write(stick(n) + "\n");
         bw.flush();
         br.close();
-        bw.close();
-
+        br.close();
     }
 }
