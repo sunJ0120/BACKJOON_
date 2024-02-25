d100cabde6a45f11c706fc2c02444018ff86fecf
sunjung Oh <sspure1214@naver.com>
2024-02-25 16:31:43 +0900

    .

--- src/자바_백준/백준_실버5/main_1181.java
+++ src/자바_백준/백준_실버5/main_1181.java
@@ -1,10 +1,11 @@
 package 자바_백준.백준_실버5;
 
 /*
-Arrays.sort의 Comparator를 재정의 하는 방법으로 해결한다.
+비교조건이 두가지나 되므로, Arrays.sort의 Comparator를 재정의 하는 방법으로 해결한다.
+- 사전순의 경우 conpareTo를 이용해 비교한다.
+- 사전순이 아닌 경우, length를 빼준다.
+- Arrays.sort의 compare의 경우, -1,0,1을 이용해 값을 비교한다.
 재정의 하는 방법은 블로그를 참조
-
-내일은 이유까지 상세하게 정리해두기
  */
 import java.io.*;
 import java.util.*;
@@ -14,34 +15,14 @@ public class main_1181 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        String[] str = new String[n];
+        int num = Integer.parseInt(br.readLine());
+        String[] str = new String[num];
 
-        //주어진 단어들 전부 넣기
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num; i++) {
             str[i] = br.readLine();
         }
 
-        //정렬
-        Arrays.sort(str, new Comparator<String>(){
-            public int compare(String str1, String str2){ //-1, 0, 1
-                if(str1.length() == str2.length()){
-                    return str1.compareTo(str2);
-                }else{
-                    return str1.length() - str2.length();
-                }
-            }
+        Arrays.sort(str,)
         });
-
-        //중복제거
-        for (int i = 1; i < n; i++) {
-            if(!str[i].equals(str[i-1])){
-                bw.write(str[i] + "\n");
-            }
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
