73ecf09491c7cea330f6327f6f681c106276ce53
sunjung Oh <sspure1214@naver.com>
2024-03-03 10:09:11 +0900

    ✅자료구조 백준 1676 실버 5
    결과 : 시도..
    - 숫자를 하나의 팩토리얼로 보고, 계속 5로 나눠서 묶어준다. 이를 통해 5의 배수가 몇개인지 이런걸 구할 수 있다.

--- src/자바_백준/백준_실버5/main_1676.java
+++ src/자바_백준/백준_실버5/main_1676.java
@@ -4,25 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*
-어렵게 짤 필요 없이. 그냥 5의 배수의 갯수에 따라 달라진다는 점을 이용하면 된다.
- */
 public class main_1676 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
         int ans = 0;
-        int m;
 
-        while(n >= 5){
-            m = n;
-            while(m % 5 == 0){ //5가 들어있는 수 구하기
-                ans++;
-                m /= 5;
-            }
-            n--;
+        while (n >= 5) {
+            ans += n / 5; //5로 나눠지는 것의 개수, 25로 나눠지는 것의 개수, 125로/..이런식인가
+            n /= 5;
         }
+
         System.out.println(ans);
     }
 }
