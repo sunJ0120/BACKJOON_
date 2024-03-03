2449f0ba5955377df1dd6ba409a7e555aac5a7e5
sunjung Oh <sspure1214@naver.com>
2024-03-03 10:05:37 +0900

    ✅자료구조 백준 1676 실버 5
    결과 : 성공
    - while문을 이용해서, 주어진 수가 5보다 클때만 수행했다.
    - 각각의 수에 대해 5가 몇번 들어가는지를 계산한다. 입력받은 n을 줄여나가며 각각의 수에 몇번 들어있는지를 더해준다.

--- src/자바_백준/백준_실버5/main_1676.java
+++ src/자바_백준/백준_실버5/main_1676.java
@@ -5,48 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
-팩토리얼 = 재귀 문제로 알고 있다.
-재귀 = dp로 해결한다.
-
-팩토리얼 구현하고, 배열로 바꾼다음 뒤에서부터 0이 아닌걸 세면 된다.
-
-*숫자 범위가 너무 커서 못구하겠음...
-
+어렵게 짤 필요 없이. 그냥 5의 배수의 갯수에 따라 달라진다는 점을 이용하면 된다.
  */
 public class main_1676 {
-    public static int[] dp;
-
-    public static int facto(int n){
-        if(n == 1 || n == 0){ //1! = 1
-            return dp[n] = 1;
-        }
-
-        if(dp[n] > 0){ //뭔가 있음
-            return dp[n];
-        }
-        return dp[n] = facto(n-1) * n;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        dp = new int[n+1];
-
-        StringBuilder stb = new StringBuilder();
-
-        facto(n);
-        stb.append(dp[n]);
-        boolean zero = false;
         int ans = 0;
+        int m;
 
-        for(int i = stb.length()-1; i >= 0;i--){ //거꾸로계산
-            if(stb.charAt(i) == '0'){ //처음 0인 숫자가 나왔을때,
-                zero = true;
+        while(n >= 5){
+            m = n;
+            while(m % 5 == 0){ //5가 들어있는 수 구하기
                 ans++;
+                m /= 5;
             }
-            if(zero && stb.charAt(i) != '0'){ //0인 상태에서 0이 아닌 것이 나왔을때
-                break;
-            }
+            n--;
         }
         System.out.println(ans);
     }
