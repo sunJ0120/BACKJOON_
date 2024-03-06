9a71c44b626fa6f9d2f254fa7a8d2fbccc6848a1
sunjung Oh <sspure1214@naver.com>
2024-03-06 14:18:09 +0900

    ✅자료구조 백준 2941 실버 5
    결과 : 실패
    - 리스트로 하고, 예외를 처리하려고 시도 했으나 실패..
    - 깔끔하게 처리하는 방법이 궁금함
    - "dz=" 랑 "z="구분법을 모르겠음.

--- src/자바_백준/백준_실버5/main_2941.java
+++ src/자바_백준/백준_실버5/main_2941.java
@@ -5,33 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
-1차 풀이 계획
-크로아티아 알파벳에 해당하는 것을 리스트로 만들어 둔다
-countChar를 이용해서 반복문을 돌면서 몇개인지를 구한다.
+"dz=" 랑 "z="구분법을 모르겠음.
  */
 public class main_2941 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
+        String str2 = str;
         int count = 0;
 
         String[] chroa = new String[]{"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
-        StringBuilder stb = new StringBuilder();
 
-        for(int j = 0; j<str.length(); j++){
-            stb.append(str.charAt(j));
-            for(int i = 0; i< chroa.length; i++){
-                if(stb.toString().contains(chroa[i])){
-                    count += ((str.length() - (str.replace(chroa[i], "")).length()) / chroa[i].length());
-                    stb.setLength(0); //초기화
-                }
-            }
-            if(stb.length() != 0){ //초기화 되지 않았다.
-                count++;
+        for(int i = 0; i< chroa.length; i++){
+            if(str.contains(chroa[i])){
+                count += ((str.length() - (str.replace(chroa[i], "")).length()) / chroa[i].length());
+                str2 = str2.replace(chroa[i], ""); //전부다 바꾸고 남은것
             }
         }
+
+        count += str2.length();
 
         System.out.println(count);
     }
