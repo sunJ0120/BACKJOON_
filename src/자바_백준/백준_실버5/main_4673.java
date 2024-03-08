39b1eada336a9263e736d11899fe7907d1697270
sunjung Oh <sspure1214@naver.com>
2024-03-08 14:29:31 +0900

    ✅자료구조 백준 4673 실버 5
    결과 : 성공
    - 희한하게 집중이 안 되었던 문제..
    - 중요한건, 시작수는 false인것만 시작을 하고
    - 점차적으로 자릿수를 더하며 다음에 만들 수를 갱신해준다.
    - 이렇게 배열을 이용하면 만들 수 없는 수가 무엇인지 인덱스를 이용해서 구할 수 있다.

--- src/자바_백준/백준_실버5/main_4673.java
+++ src/자바_백준/백준_실버5/main_4673.java
@@ -2,32 +2,35 @@ package 자바_백준.백준_실버5;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class main_4673 {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int num = 1;
+        int num;
         boolean[] self = new boolean[10000+1];
 
         for (int i = 1; i < self.length; i++) {
-            int next = 0;
-            if(!(self[i])){ //self[i]가 false 일때만
-                next += num;
-                do{
-                    next += (i %= 10); //자릿수 더하기
-                    i /= 10;
-                }while(i / 10 > 0);
+            if(!self[i]){
+                num = i; //시작수
+                int next = i;
 
-                self[next] = true;
+                while(num <= 10000){
+                    do{
+                        next += (num % 10); //자릿수 더하기
+                        num /= 10;
+                    }while(num > 0);
 
-                if(!(self[i])){ //false
-                    bw.write(i + "\n");
+                    if(next <= 10000) {
+                        self[next] = true;
+                    }
+                    num = next; //다음 num -> next 수
                 }
             }
-
+            if(!(self[i])){ //false
+                bw.write(i + "\n");
+            }
         }
 
         bw.flush();
