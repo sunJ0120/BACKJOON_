291f270f219bbebdf00e85dea476eaf3a8b0c23a
sunjung Oh <sspure1214@naver.com>
2024-04-17 14:25:32 +0900

    Create main_1158.java
    .

---
+++ src/자바_백준/백준_실버4/main_1158.java
@@ -0,0 +1,19 @@
+package 자바_백준.백준_실버4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+/*
+요세푸스 문제
+
+중요한 것은, k번째 사람을 제거하는 것인데,
+ */
+public class main_1158 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+}
