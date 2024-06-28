24a7b38e4c9ef04a47857ad6ca9af1ebf2958ac0
sunjung Oh <sspure1214@naver.com>
2024-06-28 14:49:30 +0900

    Create main_1269.java
    .

---
+++ src/자바_백준/백준_실버5/main_1269.java
@@ -0,0 +1,16 @@
+package 자바_백준.백준_실버5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+차집합을 두번 구해줄 필요 없이, 교집합을 구해서 두번 빼주자.
+ */
+public class main_1269 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
