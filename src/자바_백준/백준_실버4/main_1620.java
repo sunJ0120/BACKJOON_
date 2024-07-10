a86c335167d8ab6a7bb64bd32c1f18901851cdf8
sunjung Oh <sspure1214@naver.com>
2024-07-10 09:57:28 +0900

    [try] 2차 시도 - 성공
    * 시간초과 위험이 있으므로, hashMap을 두개 만든다.

--- src/자바_백준/백준_실버4/main_1620.java
+++ src/자바_백준/백준_실버4/main_1620.java
@@ -3,22 +3,12 @@ package 자바_백준.백준_실버4;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /*
-입력으로 들어오는 포켓몬 이름, 번호를 각각 map으로 만들고
-만약 입력으로 문자가 들어오면 번호를, 숫자가 들어오면 이름을 말하면 된다.
-
-1. 숫자 string 판별법
-2. value로 key찾기 다시 풀어야 함
-
-결과 : 시간초과..
-
-map을 두개를 선언해서 해주면 된다.....
-
-변환하고 찾는 과정에서 시간이 너무 많이 소요되기 때문에, 두개를 선언하는게 낫다.
+hashMap을 두개를 만든다.
  */
+
 public class main_1620 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,29 +19,24 @@ public class main_1620 {
         int ansloop = Integer.parseInt(st.nextToken());
 
         Map<Integer,String> pokedex = new HashMap<>();
+        Map<String,Integer> pokedexReverse = new HashMap<>();
 
         for (int i = 1; i <= poketmon; i++) {
-            st = new StringTokenizer(br.readLine());
-            pokedex.put(i, st.nextToken());
+            String str = br.readLine();
+            pokedex.put(i, str);
+            pokedexReverse.put(str, i);
         }
 
-        Set<Integer> keys = pokedex.keySet();
         for (int i = 0; i < ansloop; i++) {
-            st = new StringTokenizer(br.readLine());
+            String str = br.readLine();
             //int인지 st인지 구별
 
-            String stn = st.nextToken();
-
-            if(Character.isDigit(stn.charAt(0))){ //숫자인지 판별하기 위함
-                bw.write(pokedex.get(Integer.parseInt(stn)));
+            if(49 <= str.charAt(0) && str.charAt(0) <= 57){ //int
+                bw.write(pokedex.get(Integer.parseInt(str)));
                 bw.write("\n");
-            }else{ //string, value가 주어짐. value로 찾기
-                for(Integer key : keys){
-                    if(pokedex.get(key).equals(stn)){ //string은 equal 쓰면 안 된다.
-                        bw.write(key.toString());
-                        bw.write("\n");
-                    }
-                }
+            }else{ //string
+                bw.write(pokedexReverse.get(str).toString());
+                bw.write("\n");
             }
         }
         bw.flush();
