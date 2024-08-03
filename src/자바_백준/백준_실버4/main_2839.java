f6d81462bc942800c68bbdf8341002a2081745b4
sunjung Oh <sspure1214@naver.com>
2024-08-03 14:04:42 +0900

    [try] 2차시도 - 답안 보고 성공 수학적인 문제를 풀 때는, 규칙을 찾는 것에 주의하자.

--- src/자바_백준/백준_실버4/main_2839.java
+++ src/자바_백준/백준_실버4/main_2839.java
@@ -2,47 +2,22 @@ package 자바_백준.백준_실버4;
 
 import java.io.*;
 
-/*
-5로 먼저 나눈다
-나머지가 3으로 나눠 지는지 확인
-
-3으로 나눈다
-나눠지지 않으면 -1
- */
 public class main_2839 {
-    public static void main(String[] args) throws IOException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int num = Integer.parseInt(br.readLine());
 
-        int five_sha = num / 5;
-        int three_sha = num / 3;
-
-        int ans = 0;
-
-        if(num % 5 == 0){
-            ans = five_sha;
+        if (num == 4 || num == 7) {
+            bw.write(-1 + "\n");
+        }else if((num%5) == 0){
+            bw.write((num / 5) + "\n");
+        }else if((num%5) == 3 || (num%5) == 1){
+            bw.write(((num / 5) + 1) + "\n");
         }else{
-            int three_sh = 1;
-
-            while((5 * (five_sha)>0) && ((num - (5 * (five_sha))) %3 != 0)){
-                three_sh++; //3하나 증가
-                five_sha--; //5하나 감소
-            }
-
-            if(five_sha == 0){ //5가 없음
-                if(num % 3 == 0){
-                    ans = three_sha;
-                }else{
-                    ans = -1; //나눌 수 없음
-                }
-            }else{ //5가 하나라도 있고 끝났으므로, 값이 있음
-                ans += five_sha;
-                ans += three_sh;
-            }
+            bw.write(((num / 5) + 2) + "\n");
         }
-        bw.write(ans + "\n");
         bw.flush();
         bw.close();
         br.close();
