0762247ed583ba918a646b5c6fc2a6b4e266ad39
sunJ0120 <sspure1214@gmail.com>
2025-04-23 10:42:32 +0900

    [G5] 2493 쇠막대기_모노틱 수열 적용
    - 메모리 줄여보기
    1. 스택에 인덱스만 저장해서, 인덱스 별 순서를 보장한다.
    2. 높이를 따로 인덱스로 둔다
    3. 결과를 저장하는 배열을 하나 만들어 두고, 스택 결과에 따라 result에 저장한다.
    
    — 이렇게 하면 굳이 객체를 만들지 않고도 모노틱 수열 이용이 가능하다!

--- src/자바_백준/백준_골드5/Main2493_2.java
+++ src/자바_백준/백준_골드5/Main2493_2.java
@@ -8,78 +8,64 @@ import java.util.*;
 
 - 시간초과 해결하기
 -- O(n)이 걸리는 indexOf이 아니라, 객체를 따로 생성해서 저장하자.
+
+- 메모리 줄여보기
+1. 스택에 인덱스만 저장해서, 인덱스 별 순서를 보장한다.
+2. 높이를 따로 인덱스로 둔다
+3. 결과를 저장하는 배열을 하나 만들어 두고, 스택 결과에 따라 result에 저장한다.
+
+— 이렇게 하면 굳이 객체를 만들지 않고도 모노틱 수열 이용이 가능하다!
  */
 public class Main2493_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int roop = Integer.parseInt(br.readLine());
-
-        List<Tower> li = new ArrayList<>(); //넣을 순서를 위해 필요함. indexOf을 사용하기 위해 바꿈
+        int[] result = new int[roop];
+        int[] height = new int[roop];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i<roop; i++){
-            Tower top = new Tower(i,Integer.parseInt(st.nextToken())); //인덱스랑 높이
-            li.add(top);
+            height[i] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<Tower> sta = new Stack<>();
+        Stack<Integer> sta = new Stack<>(); //인덱스만 올리고 높이는 배열로 비교함
 
-        int ind = 0;
-        while(ind < roop){
-            if(sta.isEmpty()){
-                sta.push(li.get(ind++));
-            }else{
-                if(li.get(ind).height > sta.peek().height){ //여기는 인덱스 안움직임
-                    while(!sta.isEmpty() && li.get(ind).height > sta.peek().height){
-                        int n = sta.pop().index;
-                        if(sta.isEmpty()){
-                            break; //원래 기본 -1 넣어놨으므로
-                        }else{
-                            li.get(n).leftHeight = sta.peek().index;
-                        }
-                    }
-                }else{ //li[ind] < sta.peek() 왼쪽이 더 크다.
-                     sta.push(li.get(ind++));
+        for(int i = 0; i<roop; i++){ //height 비교
+            while(!sta.isEmpty() && height[i] > height[sta.peek()]){ //왼쪽이 더 클때까지 가야함.
+                int ind = sta.pop();
+                if(sta.isEmpty()){
+                    result[ind] = 0;
+                }else{
+                    result[ind] = sta.peek()+1; //0기반이므로...
                 }
+            }
+
+            if(sta.isEmpty()){
+                sta.push(i); //인덱스 올리기
+                continue;
+            }
+
+            if(height[i] < height[sta.peek()]){ //왼쪽이 더 크다.
+                sta.push(i);
             }
         }
 
         while(!sta.isEmpty()){
-            int n = sta.pop().index;
+            int ind = sta.pop();
             if(sta.isEmpty()){
-                break; //원래 기본 -1 넣어놨으므로
+                result[ind] = 0;
             }else{
-                li.get(n).leftHeight = sta.peek().index;
+                result[ind] = sta.peek()+1; //0기반이므로...
             }
         }
 
-        li.sort(new Comparator<Tower>() {
-            @Override
-            public int compare(Tower o1, Tower o2) {
-                return o1.index - o2.index;
-            }
-        });
-
-
-        for(Tower n : li){
-            bw.write(n.leftHeight+1 + " "); //해당 키의 값을 가져오면 변한 값 가져옴
+        for(int n : result){
+            bw.write(n + " "); //해당 키의 값을 가져오면 변한 값 가져옴
         }
         bw.write("\n");
         bw.flush();
         br.close();
         bw.close();
-    }
-}
-
-class Tower{
-    int index;
-    int height;
-    int leftHeight;
-
-    public Tower(int index, int height){
-        this.height = height;
-        this.index = index;
-        leftHeight = -1;
     }
 }
