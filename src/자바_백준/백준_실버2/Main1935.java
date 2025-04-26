4b38a9fd6e2fc0c0e02166d5abb36a4608fee1cf
sunJ0120 <sspure1214@gmail.com>
2025-04-26 19:19:47 +0900

    [S2] 1935 - string배열로 두지 않고 - stack에 double만 올리므로, double배열에 숫자만 저장하는 방식으로 변경

--- src/자바_백준/백준_실버2/Main1935.java
+++ src/자바_백준/백준_실버2/Main1935.java
@@ -10,60 +10,35 @@ public class Main1935 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int roop = Integer.parseInt(br.readLine());
-        char[] senten = br.readLine().toCharArray();
-        String[] sentence = new String[senten.length];
-        for(int i = 0; i<senten.length; i++){
-            sentence[i] = String.valueOf(senten[i]);
-        }
+        char[] sentence = br.readLine().toCharArray();
+        Double[] number = new Double[roop];
 
         Stack<Double> sta = new Stack<>();
 
-        String[] num = new String[roop];
-
         for(int i = 0; i<roop; i++){
-            num[i] = br.readLine();
-        }
-
-        //정규표현식 [A-Z]
-        String pattern = "[A-Z]";
-        //숫자 넣기
-        for(int i = 0; i<sentence.length; i++){
-            if(sentence[i].matches(pattern)){
-                sentence[i] = num[sentence[i].toCharArray()[0]-'A'];
-            }
+            number[i] = Double.parseDouble(br.readLine());
         }
 
         for(int i = 0; i<sentence.length; i++){
-            if(!sentence[i].equals("*") && !sentence[i].equals("+") && !sentence[i].equals("-")&& !sentence[i].equals("/")){
-                sta.push(Double.parseDouble(sentence[i]));
-            }
-
-            if(sentence[i].equals("*")){
+            if('A' <= sentence[i] && sentence[i] <= 'Z'){
+                sta.push(number[sentence[i]-'A']);
+            }else{
                 double a = sta.pop();
                 double b = sta.pop();
-                sta.push(a*b);
-            }
 
-            if(sentence[i].equals("+")){
-                double a = sta.pop();
-                double b = sta.pop();
-                sta.push(a+b);
-            }
-
-            if(sentence[i].equals("-")){
-                double a = sta.pop();
-                double b = sta.pop();
-                sta.push(b-a);
-            }
-
-            if(sentence[i].equals("/")){
-                double a = sta.pop();
-                double b = sta.pop();
-                sta.push(b/a);
+                if(sentence[i] == '*'){
+                    sta.push(a*b);
+                }else if(sentence[i] == '+'){
+                    sta.push(a+b);
+                }else if(sentence[i] == '-'){
+                    sta.push(b-a);
+                }else{
+                    sta.push(b/a);
+                }
             }
         }
-
         double ans = sta.pop();
-        System.out.printf("%.2f\n", ans); //표현식 주의
+        System.out.printf("%.2f\n", ans);
+        br.close();
     }
 }
